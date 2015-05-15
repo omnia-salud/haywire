81ce1b7aefeed8d35688cc1e82ad0c3962d7eec5
Pablo Fernandez <fernandezpablo85@gmail.com>
2015-05-15 15:45:23 -0300

    add deliverables

---
+++ build/haywire.js
@@ -0,0 +1,115 @@
+/// <reference path="haywire.d.ts" />
+var Haywire;
+(function (Haywire) {
+    var Pinger = (function () {
+        function Pinger(options) {
+            this.options = options;
+        }
+        Pinger.prototype.ping = function (callback) {
+            var xhr = new XMLHttpRequest();
+            xhr.timeout = this.options.timeout;
+            var status = this.options.status;
+            xhr.onreadystatechange = function (ev) {
+                if (xhr.readyState === 4) {
+                    callback(xhr.status === status);
+                }
+            };
+            xhr.open(this.options.verb, this.options.path, true);
+            xhr.send();
+        };
+        return Pinger;
+    })();
+    var CircularBuffer = (function () {
+        function CircularBuffer(size) {
+            this.size = size;
+            this.values = [];
+        }
+        CircularBuffer.prototype.add = function (value) {
+            if (this.values.length > this.size) {
+                this.values.shift();
+            }
+            this.values.push(value);
+        };
+        CircularBuffer.prototype.allOk = function () {
+            return this.values.every(function (v) { return v; });
+        };
+        CircularBuffer.prototype.allFailed = function () {
+            return this.values.every(function (v) { return !v; });
+        };
+        CircularBuffer.prototype.last = function () {
+            return this.values.slice(-1)[0];
+        };
+        return CircularBuffer;
+    })();
+    var pinger;
+    var buffer;
+    var states = [
+        { id: 0, text: "Ok" },
+        { id: 1, text: "Flaky" },
+        { id: 2, text: "Offline" },
+    ];
+    var defaults = {
+        threshold: 5,
+        ping: {
+            verb: 'GET',
+            path: '/healthcheck',
+            timeout: 1500,
+            status: 200
+        },
+        interval: 300,
+        limit: 8000
+    };
+    function _merge(a, b) {
+        for (var key in b) {
+            a[key] = b[key];
+        }
+        return a;
+    }
+    Haywire._merge = _merge;
+    function _extend() {
+        var os = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            os[_i - 0] = arguments[_i];
+        }
+        return os.reduce(function (a, b) { return _merge(a, b); }, {});
+    }
+    Haywire._extend = _extend;
+    function start(options) {
+        var opts = _extend(defaults, options);
+        buffer = new CircularBuffer(opts.threshold);
+        pinger = new Pinger(opts.ping);
+        var interval = opts.interval;
+        function updateInterval(result, last) {
+            if (result === last) {
+                return Math.min(interval * 2, opts.limit);
+            }
+            else {
+                return opts.interval;
+            }
+        }
+        function _do() {
+            setTimeout(function () {
+                pinger.ping(function (ok) {
+                    var last = buffer.last();
+                    buffer.add(ok);
+                    interval = updateInterval(ok, last);
+                    _do();
+                });
+            }, interval);
+        }
+        setTimeout(_do, 0);
+    }
+    Haywire.start = start;
+    function status() {
+        if (buffer.allOk()) {
+            return states[0];
+        }
+        else if (buffer.allFailed()) {
+            return states[2];
+        }
+        else {
+            return states[1];
+        }
+    }
+    Haywire.status = status;
+})(Haywire || (Haywire = {}));
